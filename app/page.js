acf7afdff315df5486eb58e471afa90c667270ea
Gülşah Yiğit <97913460+Gulsah-Yigit@users.noreply.github.com>
2026-08-17 12:15:12 +0300

    Use redesigned homepage on redesign branch

--- app/page.js
+++ app/page.js
@@ -1,22 +1,5 @@
-"use client";
-
-import BlogList from "@/Components/BlogList";
-import FeaturedGridFetcher from "@/Components/FeaturedGridFetcher";
-import Footer from "@/Components/Footer";
-import Header from "@/Components/Header";
-import TopNavbar from "@/Components/TopNavbar";
-import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
+import RedesignedHome from "@/Components/RedesignedHome";
 
 export default function Home() {
-  return (
-    <>
-      <ToastContainer theme="dark" />
-      <Header />
-      <TopNavbar />
-      <FeaturedGridFetcher />
-      <BlogList />
-      <Footer />
-    </>
-  );
+  return <RedesignedHome />;
 }
